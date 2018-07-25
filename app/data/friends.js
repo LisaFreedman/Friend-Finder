fed33af364e426d7bf8ab343fa9daecef119ab69
Jorjess <lisafreedman@hotmail.com>
2018-07-25 17:07:02 -0400

    add a file

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+    {
+       "name":"Erisdar The Mysterious One",
+       "photo":"https://www.pexels.com/photo/face-facial-hair-fine-looking-guy-614810/,
+       "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+       "name":"Mistical Taytay",
+       "photo":"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&h=350g",
+       "scores":[4,2,5,1,3,2,2,1,3,2]
+    },
+    {
+       "name":"Skylar Rose",
+       "photo":https://images.pexels.com/photos/871495/pexels-photo-871495.jpeg?auto=compress&cs=tinysrgb&h=350",
+       "scores":[5,2,2,2,4,1,3,2,5,5]
+    },
+    {
+       "name":"Colonel Paul",
+       "photo":"http://www.153aw.ang.af.mil/shared/media/bio/Colonel%20Paul%20Lyman.jpg",
+       "scores":[3,3,4,2,2,1,3,2,2,3]
+    },
+     {
+        "name":"Edward Snowden",
+        "photo":"https://images.pexels.com/photos/326900/pexels-photo-326900.jpeg?auto=compress&cs=tinysrgb&h=350",
+        "scores":[4,3,4,1,5,2,5,3,1,4]
+     },
+       {
+       "name":"Hesso Nader",
+       "photo":"https://images.pexels.com/photos/413727/pexels-photo-413727.jpeg?auto=compress&cs=tinysrgb&h=350",
+       "scores":[3,3,4,1,4,3,5,3,5,4]
+    },
+     {
+        "name":"Sonneth The Swift",
+        "photo":"https://images.pexels.com/photos/40997/mona-lisa-leonardo-da-vinci-la-gioconda-oil-painting-40997.jpeg?auto=compress&cs=tinysrgb&h=350",
+        "scores":[4,4,2,3,2,2,3,2,4,5]
+     }
+ 
+ ]
+ 
+ module.exports = friends;
