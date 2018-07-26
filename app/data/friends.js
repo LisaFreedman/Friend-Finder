3aa8a56e2691c273fd906e9ba82842868a9c36b0
Jorjess <lisafreedman@hotmail.com>
2018-07-26 17:18:59 -0400

    final commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,36 @@
 var friends = [
     {
-       "name":"Erisdar The Mysterious One",
-       "photo":"https://www.pexels.com/photo/face-facial-hair-fine-looking-guy-614810/,
+       "name":"Jordan Pollock",
+       "photo":"https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
        "scores":[5,1,4,4,5,1,2,5,4,1]
     },
     {
-       "name":"Mistical Taytay",
-       "photo":"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&h=350g",
+       "name":"Ida Player",
+       "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs0ZHqYXVYHuioXy5denNWXXii9tP-fqtNlzFJJ2AJTkwDrtz40A",
        "scores":[4,2,5,1,3,2,2,1,3,2]
     },
     {
-       "name":"Skylar Rose",
-       "photo":https://images.pexels.com/photos/871495/pexels-photo-871495.jpeg?auto=compress&cs=tinysrgb&h=350",
+       "name":"Sally Smithsonian",
+       "photo":"https://images.pexels.com/photos/871495/pexels-photo-871495.jpeg?auto=compress&cs=tinysrgb&h=350",
        "scores":[5,2,2,2,4,1,3,2,5,5]
     },
     {
-       "name":"Colonel Paul",
-       "photo":"http://www.153aw.ang.af.mil/shared/media/bio/Colonel%20Paul%20Lyman.jpg",
+       "name":"George Lazenbyl",
+       "photo":"https://i.pinimg.com/736x/8d/23/5f/8d235f1603100bc5ab4e78de36e1b110--mens-headshots-young-man.jpg",
        "scores":[3,3,4,2,2,1,3,2,2,3]
     },
      {
-        "name":"Edward Snowden",
+        "name":"Betty Bird",
         "photo":"https://images.pexels.com/photos/326900/pexels-photo-326900.jpeg?auto=compress&cs=tinysrgb&h=350",
         "scores":[4,3,4,1,5,2,5,3,1,4]
      },
        {
-       "name":"Hesso Nader",
+       "name":"Hilde Hatfield",
        "photo":"https://images.pexels.com/photos/413727/pexels-photo-413727.jpeg?auto=compress&cs=tinysrgb&h=350",
        "scores":[3,3,4,1,4,3,5,3,5,4]
     },
      {
-        "name":"Sonneth The Swift",
+        "name":"Lisa Mona",
         "photo":"https://images.pexels.com/photos/40997/mona-lisa-leonardo-da-vinci-la-gioconda-oil-painting-40997.jpeg?auto=compress&cs=tinysrgb&h=350",
         "scores":[4,4,2,3,2,2,3,2,4,5]
      }
